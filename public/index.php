9b5b5e29c8270bda6f3823b5df1c75e8080bca62
IgorOlikov <i.olikov@mail.ru>
2024-01-17 21:40:16 +0500

    di-container,twig,doctrine

--- public/index.php
+++ public/index.php
@@ -1,19 +1,35 @@
 <?php
+
+use App\Controllers\HomeController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Views\Twig;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$container = new Container();
+
+$container->set('Twig', Twig::class);
+
+AppFactory::setContainer($container);
+
 
 $app = AppFactory::create();
 
 
-$app->get('/', function (Request $request, Response $response, $args) {
+//dd($app->getContainer());
+
+
+$app->get('/', function (Request $request, Response $response, $args,Twig $twig) {
     $response->getBody()->write("Hello world!");
-
+    //$twig->render();//
     return $response;
-
-
 });
+
+$app->get('/home',[HomeController::class,'index']);
+
+
 
 $app->run();
---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+class HomeController
+{
+    public function index(Request $request,Response $response, $args)
+    {
+        dd($request);
+
+    }
+
+
+}
